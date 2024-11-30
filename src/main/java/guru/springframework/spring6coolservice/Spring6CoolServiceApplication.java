16958a5534d3e281957915f312f91aa6c732f9b7
John Thompson <john@springframework.guru>
2024-11-30 07:05:59 -0500

    Initial Project Add

---
+++ src/main/java/guru/springframework/spring6coolservice/Spring6CoolServiceApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring6coolservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6CoolServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring6CoolServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframework/spring6coolservice/Spring6CoolServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring6coolservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6CoolServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
